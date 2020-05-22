4a32b9b68350a9b36a72cbc94a304163fa7e9538
妹尾弦 <gengineer.web@gmail.com>
2020-05-22 20:32:55 +0900

    feat: 投稿ページ作成(あとでリファクタリングすること)

--- app/components/Atoms/ButtonForUpload.vue
+++ app/components/Atoms/ButtonForUpload.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn block color="brown lighten-1" dark>
+  <v-btn block color="brown lighten-1" dark @click="clicked()">
     <v-icon left>mdi-dog</v-icon>
     シェアする
     <v-icon right>mdi-pen-plus</v-icon>
@@ -8,7 +8,13 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  methods: {
+    clicked() {
+      this.$emit('emit-clicked')
+    },
+  },
+})
 </script>
 
 <style lang="scss" scoped></style>
